03aff8cebb8622c551cd486480fd59ea6c603d68
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-08-05 17:51:57 +0900

    ログイン、ログアウトの追加、

---
+++ next-app/app/api/user/show/route.ts
@@ -0,0 +1,51 @@
+import { db } from "@/lib/prisma";
+import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req : NextRequest) {
+  try {
+    const supabase = await createClient();
+    const {data , error} = await supabase.auth.getUser();
+
+    if (error) {
+        return NextResponse.json(
+        {message : error.message}, // 認証エラー
+        {status : 401}
+        )
+    }
+
+    // ユーザーIDを取得
+    const userId = data.user?.id;
+    if(!userId){
+        return NextResponse.json(
+        {message : "ユーザーIDが取得できませんでした"},
+        {status : 400}
+        )
+    }
+
+    const userDetail = await db.user.findUnique({
+        where : {
+        id : userId
+        },
+        select : {
+        name : true,
+        email : true,
+        userIcon : true,
+        }
+    });
+
+    if(!userDetail){
+        return NextResponse.json(
+        {message : "ユーザーが見つかりませんでした"},
+        {status : 404}
+        )
+    }
+
+    return NextResponse.json(userDetail);
+} catch (error) {
+  return NextResponse.json(
+    {message : "ユーザー情報の取得に失敗しました"},
+    {status : 500}
+  )
+}
+}
--- next-app/app/login/page.tsx
+++ next-app/app/login/page.tsx
@@ -1,9 +1,50 @@
 'use client';
-
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 export default function Login() {
-    return (
-        <main>
-            <h1>ログイン</h1>
-        </main>
-    )
+  const [email , setEmail] = useState<string>("");
+  const [password , setPassword] = useState<string>("");
+  const [isLoading , setIsLoading] = useState<boolean>(false);
+  // クライアントからsupabaseにログインをリクエスト
+  const router = useRouter();
+  const supabase = createClient();
+
+  // ログイン
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+     try {
+        const {data , error} = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        if(error) {
+            throw new Error(error.message);
+        }
+        router.push('/');
+        setIsLoading(false);
+     } catch (error) {
+        console.log(error);
+     }
+     finally {
+        setIsLoading(false);
+     }
+  };
+
+
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit} className="p-4 bg-yellow-50">
+        <h2>メールアドレス</h2>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="p-4  rounded-2xl"/>
+        <h2>パスワード</h2>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="p-4  rounded-2xl"/>
+        <button type="submit" disabled={isLoading} className="p-4 rounded-2xl">
+          {isLoading ? "ログイン中..." : "ログイン"}
+        </button>
+      </form>
+    </main>
+  )
 }
