692addeb7239ea78dd5915a3832fc9a122899d4a
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-08-07 01:04:08 +0900

    SearchBarコンポーネントの作成 見た目と入力のみ可能

--- next-app/app/test/page.tsx
+++ next-app/app/test/page.tsx
@@ -3,12 +3,13 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import {FooterNavItem} from "@/components/shared/FooterNavItem"
+import { FooterNavItem } from "@/components/shared/FooterNavItem";
+import { SearchBar } from "@/components/shared/SearchBar";
 
-export default function Test() {
-    return (
-        <>
-            <FooterNavItem />
-        </>
-    );
-}           
+// export default function Test() {
+//   return (
+//     <>
+//   <SearchBar/>
+//   </>
+//   )
+// }
