9204aaf700076f62c103dd3dfce89843eacca06c
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-08-07 11:29:26 +0900

    ファイルを隠しsearch-resultsの準備

---
+++ next-app/app/search-results/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import Link from "next/link";
+import Image from "next/image";
+
+import {SearchBar} from "@/components/shared/SearchBar";
+import { SearchShopCard } from "@/components/features/SearchShopCard";
