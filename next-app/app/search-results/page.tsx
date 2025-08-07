2d744ddfe721e0111a93434f3f91c3127e5e7c69
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-08-07 14:24:48 +0900

    複数コンポーネントの修正　Search-Resultsページの見た目の完成

--- next-app/app/search-results/page.tsx
+++ next-app/app/search-results/page.tsx
@@ -5,11 +5,32 @@ import Image from "next/image";
 
 import { SearchBar } from "@/components/shared/SearchBar";
 import { SearchShopCard } from "@/components/features/SearchShopCard";
+import { FooterNavItem } from "@/components/shared/FooterNavItem";
 
 export default function SearchResults() {
   return (
     <>
-      <SearchBar />
+                <div className="flex flex-col items-center justify-center  w-screen gap-4 fixed top-0, left-0, w-full, z-index bg-[#FFF] pb-4">
+                    <SearchBar/>
+                    <p className="text-lg font-bold text-[#8B4158] border-b-2 border-solid border-[#8B4158] ">検索結果</p>
+                </div>
+
+                <div className="flex flex-col items-center justify-center  wt-full gap-4 pt-30 ">
+                    <SearchShopCard ImageType="shop1"/>
+                    <SearchShopCard ImageType="shop2"/>
+                    <SearchShopCard ImageType="shop1"/>
+                    <SearchShopCard ImageType="shop2"/>
+                    <SearchShopCard ImageType="shop1"/>
+                    <SearchShopCard ImageType="shop2"/>
+                    <SearchShopCard ImageType="shop1"/>
+                    <SearchShopCard ImageType="shop2"/>
+                    <SearchShopCard ImageType="shop1"/>
+                    <SearchShopCard ImageType="shop2"/>
+                </div>
+
+                <div className="flex flex-col items-center justify-center fixed bottom-6 left-0, w-full">
+                <FooterNavItem/>
+                </div>
     </>
   );
 }
