d7f2287f5d1bb2f470ae49cf51201580b92e0e74
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-08-08 19:03:14 +0900

    dev確認

--- next-app/app/components/store/get-store.tsx
+++ next-app/app/components/store/get-store.tsx
@@ -3,6 +3,8 @@
 import { Store } from "@/app/types/store";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Fetching from "@/app/components/common/Fetching";
+import { BackButton } from "@/components/features/BackButton";
 
 export default function GetStore() {
   const [store, setStore] = useState<Store | null>(null);
@@ -28,10 +30,13 @@ export default function GetStore() {
   return (
     <div>
       {isLoading ? (
-        <div>Loading...</div>
+        <Fetching />
       ) : (
-        <div>
-          <div>店名：{store?.name}</div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center justify-center gap-2">
+            <BackButton />
+            <h2 className="text-[16px] font-bold text-[#8B4158]">{store?.name}</h2>
+          </div>
           <div>住所：{store?.address}</div>
           <div>ジャンル：{store?.genre.name}</div>
           <div>画像：{store?.storeImage?.[0].path}</div>
